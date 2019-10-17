e2ef202a99c124af50602445e6372f495760b05a
Stefanie Davis <davisstefanie2010@gmail.com>
2019-10-17 05:06:05 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,6 @@
 class Dog
-  
   def initialize(name, breed="Mutt")
     @name = name
     @breed = breed
   end
- 
-  # def name=(name)
-  #   @name = name
-  # end
- 
-  # def name
-  #   @name
-  # end
 end 
